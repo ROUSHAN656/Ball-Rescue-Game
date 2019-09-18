f2ba1c0edc09251595d6b0ac8d40d1c0c6f1b828
satyajitdash61 <32846765+satyajitdash61@users.noreply.github.com>
2019-09-18 18:15:43 +0530

    done with the pad moving

--- game.js
+++ game.js
@@ -5,6 +5,61 @@ canvas.height=500;
 let c=canvas.getContext("2d");
 
  c.fillStyle="rgba(255,0,0,0.5)";
+
+
+let x=200;
+let y=200;
+let pad_x=0;
+let pad_dx=20;
+let dx=4;
+let dy=4;
+let radius=30;
+let innerHeight=500;
+let innerWidth=900;
+function animate(){
+    c.clearRect(0,0,innerWidth,innerHeight); 
+    requestAnimationFrame(animate);
+    c.fillRect(pad_x,475,100,20);
+    document.onkeydown=move;
+    function move(e){
+        // console.log(e.keyCode);
+        if(e.keyCode==39){
+            pad_x+=pad_dx;
+        }
+        if(e.keyCode==37){
+            pad_x-=pad_dx;
+        }
+
+        canvas.width=900;
+        c.fillRect(pad_x,475,100,20);
+
+    }
+    c.beginPath();
+    c.arc(x,y,radius,0,Math.PI*2,false);
+    c.strokeStyle='red';
+    c.stroke();
+    
+if(x+radius>innerWidth || x-radius < 0){
+    dx=-dx;
+}
+if(y+radius>innerHeight || y-radius < 0){
+    dy=-dy;
+}
+
+    x+=dx;
+    y+=dy;
+    // console.log('pop');
+}
+
+
+animate();
+
+
+
+
+
+
+
 // c.fillRect(100,100,100,100);
 // c.fillStyle="rgba(0,0,255,0.5)";
 // c.fillRect(300,300,100,100);
@@ -32,34 +87,3 @@ let c=canvas.getContext("2d");
 // c.strokeStyle='red';
 // c.stroke();
 // }
-
-let x=200;
-let y=200;
-let dx=4;
-let dy=4;
-let radius=30;
-let innerHeight=500;
-let innerWidth=900;
-function animate(){
-    c.clearRect(0,0,innerWidth,innerHeight); 
-    requestAnimationFrame(animate);
-    c.fillRect(0,475,100,20);
-    c.beginPath();
-    c.arc(x,y,radius,0,Math.PI*2,false);
-    c.strokeStyle='red';
-    c.stroke();
-    
-if(x+radius>innerWidth || x-radius < 0){
-    dx=-dx;
-}
-if(y+radius>innerHeight || y-radius < 0){
-    dy=-dy;
-}
-
-    x+=dx;
-    y+=dy;
-    // console.log('pop');
-}
-
-
-animate();
