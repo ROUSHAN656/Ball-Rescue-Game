f5cb0cd7350817e455d09d95bcc8c6aa6c134386
ROUSHAN RAJ <roushan.raj656@gmail.com>
2019-09-21 13:56:58 +0530

    changed canvas,added high score

--- game.js
+++ game.js
@@ -2,8 +2,22 @@
 // // Game Title:- Ball Rescue
 
 let canvas=document.querySelector("canvas");
-canvas.width=900;
-canvas.height=600;
-let c=canvas.getContext("2d");
-c.fillStyle="teal";
-let x= Math.random() * canvas.width;
+canvas.height=500;
+canvas.width=1000;
+let c =canvas.getContext("2d");
+
+c.fillStyle="rgba(255,0,0,0.4)";
+let x=Math.random()*canvas.width;
+let y=100;
+let pad_x=400;
+let pad_dx=20;
+let dx=3;
+let dy=3;
+let radius=30;
+let innerHeight=500;
+let innerWidth=900;
+let newScore=0;
+
+if(localStorage.hasOwnProperty('hs')){
+    document.getElementById("highScore").innerHTML=parseInt(localStorage.getItem("hs"));
+}
