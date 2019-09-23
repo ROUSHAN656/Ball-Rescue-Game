301d8989d1e9ac4272922747001e6621463fa9ce
ROUSHAN RAJ <roushan.raj656@gmail.com>
2019-09-23 09:59:51 +0530

    Update game.js

--- game.js
+++ game.js
@@ -9,46 +9,36 @@ class canv{
    this.c1.fillStyle="rgba(255,0,0,0.5)";
    this.x=Math.random()* this.canvas.width;
 
-let y=100;
-let pad_x=400;
-let pad_dx=20;
-let dx=3;
-let dy=3;
-let radius=30;
-let innerHeight=500;
-let innerWidth=900;
-var newScore=0;
+   this.y=100;
+   this.pad_x=400;
+   this.pad_dx=20;
+   this.dx=3;
+   this.dy=3;
+   this.radius=30;
+   this.innerHeight=500;
+   this.innerWidth=900;
+   this.newScore=0;
+      
 // storing the high score in local storage
 if(localStorage.hasOwnProperty('hs')){
     document.getElementById("highScore").innerHTML=parseInt(localStorage.getItem("hs"));
 }
 
 // function for the movements
-animate=()=>{
-    c.clearRect(0,0,innerWidth,innerHeight); 
-    requestAnimationFrame(animate);
-    c.fillRect(pad_x,480,100,20);
-    document.onkeydown=move;
-    function move(e){
-        if(e.keyCode==39){           
-            if(pad_x<800){
-                pad_x+=pad_dx;
-            }
-        }
-        if(e.keyCode==37){
-            if(pad_x>0){
-            pad_x-=pad_dx;
-            }
-        }
-        canvas.width=900;
-        c.fillRect(pad_x,475,100,20);    
-}
-    c.beginPath();
-    c.arc(x,y,radius,0,Math.PI*2,false);
-    c.fillStyle="red";
-    c.fill();
-    c.strokeStyle='red';
-    c.stroke();
+  this.animate = function(){
+       console.log(this.c1);
+       this.c1.clearRect(0,0,this.innerWidth,this.innerHeight);
+       // this.anim=this.animate;
+       // requestAnimationFrame(this.anim);
+       this.c1.fillRect(this.pad_x,480,100,20);
+       this.c1.beginPath();
+       this.c1.arc(this.x,this.y,this.radius,0,Math.PI*2,false);
+       this.c1.fillStyle="red";
+       this.c1.fill();
+       this.c1.strokeStyle='red';
+       this.c1.stroke();     
+      
+      
     
 if(x+radius>innerWidth || x-radius < 0){
     dx=-dx;
